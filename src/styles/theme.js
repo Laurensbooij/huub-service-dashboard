8d475062dd1bab611465e88bfe5fc01332d0a61f
Laurens Booij <laurens.booij@labela.nl>
2019-07-31 12:54:26 +0200

    Add colors to theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,20 @@
 export default {
-  prime: '#e7ff00',
-  black: '#000000',
+  colors: {
+    primary: {
+      midnightblue: '#0B0C1E',
+      aquablue: '#4EE3EA',
+    },
+    notification: {
+      correct: '#99DD55',
+      warning: '#FFAA22',
+      error: '#FF0000',
+    },
+    accent: {
+      pearl: '#F8F9F9',
+      pale: '#F2F4F4',
+      lightgrey: '#E7E7E9',
+      grey: '#919199',
+    },
+    white: '#FFFFFF',
+  },
 };

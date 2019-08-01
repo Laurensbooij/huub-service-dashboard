7ea38ee830a0ff3f8f09aca54451a467ad8ddc82
Laurens Booij <laurens.booij@labela.nl>
2019-08-01 17:12:40 +0200

    Add VehicleOVerview component

---
+++ src/components/modules/VehicleOverview/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import PageHeaderBarContainer from 'common/PageHeaderBarContainer';
+import VehicleTypeList from './VehicleTypeList';
+
+const VehicleOverview = () => (
+  <PageHeaderBarContainer
+    to="/"
+    title="Voertuigen"
+    subTitle=""
+  >
+    <VehicleTypeList/>
+  </PageHeaderBarContainer>
+
+);
+
+export default VehicleOverview;

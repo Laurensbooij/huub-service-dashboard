b63458bce88a8006c6821072732d687447fbd0fe
Laurens Booij <laurens.booij@labela.nl>
2019-08-01 10:41:27 +0200

    Add HeaderBar component

---
+++ src/components/common/HeaderBar/ActionButton/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import PT from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import ArrowBackSvg from 'app/static/vectors/arrow-back.svg';
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  position: absolute;
+  left: 0;
+  width: 60px;
+  height: 100%;
+`;
+
+const Icon = styled(ArrowBackSvg)`
+  transform: scale(0.9);
+  margin: 0 0 0 16px;
+`;
+
+const ActionButton = ({ to }) => (
+  <StyledLink to={to}>
+    <Icon />
+  </StyledLink>
+);
+
+ActionButton.propTypes = {
+  to: PT.string.isRequired,
+};
+
+export default ActionButton;
---
+++ src/components/common/HeaderBar/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import PT from 'prop-types';
+
+import ActionButton from './ActionButton';
+
+const Container = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100vw;
+  height: 60px;
+  background-color: ${(props) => props.theme.colors.primary.midnightblue};
+  z-index: 10;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 20px;
+  text-transform: capitalize;
+  margin: 12px 0 6px;
+  color: ${(props) => props.theme.colors.white};
+`;
+
+const SubTitle = styled.h3`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 14px;
+  margin: 0;
+  color: ${(props) => props.theme.colors.white};
+`;
+
+const HeaderBar = ({ to, title, subTitle }) => (
+  <Container>
+    <ActionButton to={to}/>
+    <Title>{title}</Title>
+    <SubTitle>{subTitle}</SubTitle>
+  </Container>
+);
+
+HeaderBar.propTypes = {
+  to: PT.string,
+  title: PT.string,
+  subTitle: PT.string,
+};
+
+export default HeaderBar;

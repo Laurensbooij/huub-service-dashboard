a77a727b82bed281f89baf257ee3039020df4207
Laurens Booij <laurens.booij@labela.nl>
2019-08-01 16:36:02 +0200

    Add Paragraph and Heading3 component

---
+++ src/components/common/Heading3/index.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.h3`
+  font-size: 18px;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.primary.midnightblue};
+  margin: 0;
+`;
---
+++ src/components/common/Paragraph/index.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export default styled.p`
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.primary.midnightblue};
+`;

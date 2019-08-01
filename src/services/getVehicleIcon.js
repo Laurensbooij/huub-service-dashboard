4332e0ba4836389495ecd9b7b51f8f842efde405
Laurens Booij <laurens.booij@labela.nl>
2019-08-01 16:31:00 +0200

    Add service to select vehicle icon

---
+++ src/services/getVehicleIcon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PT from 'prop-types';
+
+import CarIconSvg from 'app/static/vectors/vehicles/vehicle-car.svg';
+import ScooterIconSvg from 'app/static/vectors/vehicles/vehicle-scooter.svg';
+import BikeIconSvg from 'app/static/vectors/vehicles/vehicle-bike.svg';
+
+
+const getBatteryIcon = (vehicleType) => {
+  if (vehicleType === 'middenklasser') {
+    return <CarIconSvg/>;
+  }
+  if (vehicleType === 'e-scooter') {
+    return <ScooterIconSvg/>;
+  }
+  if (vehicleType === 'e-bike') {
+    return <BikeIconSvg/>;
+  }
+};
+
+getBatteryIcon.propTypes = {
+  vehicleType: PT.string,
+
+};
+
+export default getBatteryIcon;

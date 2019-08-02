96199daa7c597bcfee44e277599509114f2e3332
Laurens Booij <laurens.booij@labela.nl>
2019-08-02 13:53:29 +0200

    Add mock data

---
+++ src/services/data/vehicles.js
@@ -0,0 +1,215 @@
+export default {
+  'schoonschip': {
+    huubSpotName: 'Schoonschip',
+    vehicles: {
+      'middenklasser': [
+        {
+          vehicleId: 'ABC-1-A',
+          mainBatteryPercentage: 91,
+          lockBatteryPercentage: 53,
+        },
+        {
+          vehicleId: 'AAC-2-B',
+          mainBatteryPercentage: 64,
+          lockBatteryPercentage: 97,
+        },
+        {
+          vehicleId: 'ABB-1-C',
+          mainBatteryPercentage: 65,
+          lockBatteryPercentage: 73,
+        },
+        {
+          vehicleId: 'BAB-3-C',
+          mainBatteryPercentage: 20,
+          lockBatteryPercentage: 46,
+        },
+        {
+          vehicleId: 'CCA-2-A',
+          mainBatteryPercentage: 87,
+          lockBatteryPercentage: 20,
+        },
+      ],
+      'e-scooter': [
+        {
+          vehicleId: 'S 1',
+          mainBatteryPercentage: 60,
+          lockBatteryPercentage: 64,
+        },
+        {
+          vehicleId: 'S 2',
+          mainBatteryPercentage: 74,
+          lockBatteryPercentage: 30,
+        },
+      ],
+      'e-bike': [
+        {
+          vehicleId: 'E 1',
+          mainBatteryPercentage: 72,
+          lockBatteryPercentage: 44,
+        },
+        {
+          vehicleId: 'E 2',
+          mainBatteryPercentage: 74,
+          lockBatteryPercentage: 80,
+        },
+        {
+          vehicleId: 'E 3',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 10,
+        },
+      ],
+    },
+  },
+  'hoog-boulandt': {
+    huubSpotName: 'Hoog Boulandt',
+    vehicles: {
+      'middenklasser': [
+        {
+          vehicleId: 'DEA-3-B',
+          mainBatteryPercentage: 91,
+          lockBatteryPercentage: 53,
+        },
+        {
+          vehicleId: 'EBA-1-A',
+          mainBatteryPercentage: 64,
+          lockBatteryPercentage: 97,
+        },
+        {
+          vehicleId: 'ACC-4-C',
+          mainBatteryPercentage: 65,
+          lockBatteryPercentage: 73,
+        },
+        {
+          vehicleId: 'BEB-3-B',
+          mainBatteryPercentage: 20,
+          lockBatteryPercentage: 46,
+        },
+        {
+          vehicleId: 'EBA-2-A',
+          mainBatteryPercentage: 87,
+          lockBatteryPercentage: 20,
+        },
+      ],
+      'e-scooters': [
+        {
+          vehicleId: 'S 1',
+          mainBatteryPercentage: 80,
+          lockBatteryPercentage: 54,
+        },
+      ],
+      'e-bike': [
+        {
+          vehicleId: 'E 1',
+          mainBatteryPercentage: 62,
+          lockBatteryPercentage: 84,
+        },
+        {
+          vehicleId: 'E 2',
+          mainBatteryPercentage: 84,
+          lockBatteryPercentage: 30,
+        },
+        {
+          vehicleId: 'E 3',
+          mainBatteryPercentage: 100,
+          lockBatteryPercentage: 100,
+        },
+      ],
+    },
+  },
+  'olympisch-stadion': {
+    huubSpotName: 'Olympisch stadion',
+    vehicles: {
+      'middenklasser': [
+        {
+          vehicleId: 'AFD-4-A',
+          mainBatteryPercentage: 83,
+          lockBatteryPercentage: 43,
+        },
+        {
+          vehicleId: 'ABD-2-C',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 97,
+        },
+        {
+          vehicleId: 'AEA-3-A',
+          mainBatteryPercentage: 24,
+          lockBatteryPercentage: 43,
+        },
+        {
+          vehicleId: 'BBB-5-C',
+          mainBatteryPercentage: 80,
+          lockBatteryPercentage: 76,
+        },
+        {
+          vehicleId: 'ACD-3-A',
+          mainBatteryPercentage: 77,
+          lockBatteryPercentage: 30,
+        },
+      ],
+      'e-scooters': [
+        {
+          vehicleId: 'S 1',
+          mainBatteryPercentage: 12,
+          lockBatteryPercentage: 54,
+        },
+        {
+          vehicleId: 'S 2',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 30,
+        },
+        {
+          vehicleId: 'S 3',
+          mainBatteryPercentage: 64,
+          lockBatteryPercentage: 54,
+        },
+        {
+          vehicleId: 'S 4',
+          mainBatteryPercentage: 70,
+          lockBatteryPercentage: 30,
+        },
+      ],
+      'e-bike': [
+        {
+          vehicleId: 'E 1',
+          mainBatteryPercentage: 72,
+          lockBatteryPercentage: 44,
+        },
+        {
+          vehicleId: 'E 2',
+          mainBatteryPercentage: 74,
+          lockBatteryPercentage: 80,
+        },
+        {
+          vehicleId: 'E 3',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 10,
+        },
+        {
+          vehicleId: 'E 4',
+          mainBatteryPercentage: 64,
+          lockBatteryPercentage: 30,
+        },
+        {
+          vehicleId: 'E 5',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 50,
+        },
+        {
+          vehicleId: 'E 6',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 60,
+        },
+        {
+          vehicleId: 'E 7',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 43,
+        },
+        {
+          vehicleId: 'E 8',
+          mainBatteryPercentage: 94,
+          lockBatteryPercentage: 76,
+        },
+      ],
+    },
+  },
+};
